3fc760b5393ba17821cb1caf26de7f2283af85b8
DavidSenn <178643637+DavidStaus@users.noreply.github.com>
2024-10-05 18:32:19 +0200

    finish 07 exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+// x °F ≘ (x − 32) × ⁠5/9⁠ °C 
+// to 1 decimial: 
+// const num = 3.14159;
+// const roundedNum = Math.round(num * 10) / 10; 
+// console.log(roundedNum); // Output: 3.1
+const convertToCelsius = function(tempF) {
+  return Math.round((tempF - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+// x °C ≘ (x × ⁠9/5⁠ + 32) °F 
+const convertToFahrenheit = function(tempC) {
+  return Math.round((tempC * (9 / 5) + 32) * 10) / 10
 };
 
 // Do not edit below this line
