728a717198c0e1ff1caf89f7cc857509136d011a
DavidSenn <178643637+DavidStaus@users.noreply.github.com>
2024-10-19 01:57:10 +0200

    solved 10_fibonnaci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS";
+    }
+    let fib = 0;
+    let firstNum = 0;
+    let secondNum = 1;
+    for (let i = 2; i <= n; i++) {
+        fib = firstNum + secondNum;
+        firstNum = secondNum;
+        secondNum = fib;
+    }
+    return fib
 };
+
+// first add 1 and 0, then 1 and 1, and then 1 and 2, then 2 and 3, 3 and 5, then 
 
 // Do not edit below this line
 module.exports = fibonacci;
+
