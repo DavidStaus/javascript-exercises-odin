88498e1307d2ecb3352676e04d0780caf031573e
DavidSenn <178643637+DavidStaus@users.noreply.github.com>
2024-10-20 10:52:30 +0200

    refactor solution to 12_

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,21 +1,16 @@
+const getAge = function (person) {
+    person.yearOfDeath ??= new Date().getFullYear();
+    
+    return person.yearOfDeath - person.yearOfBirth
+  };
+
 const findTheOldest = function(people) {
-    // for each object in the array calculate the age by subtracting yearOfBirth from their yearOfDeath
-    people.forEach((person) => {
-            if (person.yearOfDeath) {
-                person.age = person.yearOfDeath - person.yearOfBirth;
-            } else {
-                person.age = new Date().getFullYear() - person.yearOfBirth;
-            }
-        })
-    people.sort((a, b) => b.age - a.age);
-    return people[0];
-    // return people.map((person) => {
-    //     if (person.yearOfDeath) {
-    //         person.yearOfDeath - person.yearOfBirth;
-    //     } else {
-    //         new Date().getFullYear() - person.yearOfBirth;
-    //     }
-    // });
+    const peopleOldestToYoungest = people.toSorted(
+        (person, nextPerson) => getAge(nextPerson) - getAge(person),
+    );
+    
+    const oldestPerson = peopleOldestToYoungest[0]
+    return oldestPerson;
 };
 
 // Do not edit below this line
